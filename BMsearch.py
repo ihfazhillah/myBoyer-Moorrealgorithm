211a2eb6025032f1eb91d8bd8f29d27cff0ee379
unknown <dagger003@163.com>
2013-12-06 19:32:04 +0800

    finish version1.0

--- BMsearch.py
+++ BMsearch.py
@@ -6,32 +6,47 @@ s_len=0
 d_len=0
 
 def use_bad_table((wrong_word,s_now)):
-    return 7
+    if wrong_word in bad_table:
+        for index in bad_table[wrong_word][::-1]:
+            if s_now>index:
+                return s_now-index
+    return s_now+1
     
-def use_good_table((wrong_word,s_now)):
+def use_good_table(src,(wrong_word,s_now)):
+    s_now += 1
+    indexes = good_table[src[s_now:]]
+    if indexes != []:
+    #the tail--src[s_now:] have appearience before
+        for index in indexes[::-1]:
+            if s_now>index:
+                return s_len-1-index
+    else:
+        for s_now in range(s_now+1,s_len): 
+            indexes = good_table[src[s_now:]]
+            if (indexes is not []) and ((s_len-s_now-1) in indexes):
+                return s_now
     return 1
     
 def check_word(src,dst,total_position):
+    '''after each move,check a char from end'''
     global bad_table
     global good_table
     global s_len
-    global d_len
     
     s_now = s_len-1
-    dic=dict.fromkeys(src,1)
-    if src[-1] != dst[total_position]:
-        if src[-1] not in dic:
+    if src[-1] != dst[total_position]:#no tail
+        if dst[total_position] not in bad_table:
             return 1,s_len
-        else :
-            return 2,(src[-1],s_now)
-    else:
+        else:
+            return 2,(dst[total_position],s_now)
+    else:#have a tail
         d_now = total_position
-        while s_now>=0:
+        while s_now>0:
             s_now -= 1
             d_now -= 1
             if src[s_now] != dst[d_now]:
                 wrong_word = dst[d_now]
-                return 2,(wrong_word,s_now)
+                return 3,(wrong_word,s_now)
                 
     return 0,0
     
@@ -48,7 +63,7 @@ def create_bad_table(src):
 def create_good_table(src):
     global good_table
     lst=[]
-    for temp in range(s_len/2,s_len):
+    for temp in range(1,s_len):
         lst.append(src[temp:])
     good_table=dict.fromkeys(lst) 
     for key in good_table.keys():
@@ -71,7 +86,7 @@ def BMsearch(src,dst):
     global d_len
     
     s_len=len(src)
-    d_len=len(dst)
+    d_len=len(dst)-1
     total_position=s_len-1
 
     create_bad_table(src)
@@ -81,10 +96,13 @@ def BMsearch(src,dst):
         if flag == 0:#find
             return total_position-s_len+1  
         elif flag == 1:#all move
-            total_position+=result
+            total_position += result
         elif flag == 2:
             bad_dis = use_bad_table(result)
-            good_dis = use_good_table(result)
+            total_position += bad_dis
+        elif flag == 3:
+            bad_dis = use_bad_table(result)
+            good_dis = use_good_table(src,result)
             if bad_dis > good_dis:
                 total_position += bad_dis
             else:
@@ -97,4 +115,4 @@ if __name__ == "__main__":
     source="EXAMPLE"
     dest="HERE IS A SIMPLE EXAMPLE"
     result=BMsearch(source,dest)
-#    print result
+    print result
